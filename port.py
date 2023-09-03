93d14fe327cb5ef14250c1da3f547a0bac1a6000
Mare <2548098501@qq.com>
2023-09-03 23:02:52 +0800

    init commit

---
+++ port.py
@@ -0,0 +1,10 @@
+import socket
+
+class Pp:
+
+    def __init__(self):
+        # try:
+        #     self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # except socket.error, msg:
+        #     print("Failed to create socket. Error code:"  + str(msg[0]) + " , Error message :"  + msg[1])
+        #     sys.exit();
