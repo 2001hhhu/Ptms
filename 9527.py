1a14d8dcff9fc717e6b68b3230185b71a6ad5a56
9527 <1537877786@qq.com>
2023-09-04 22:17:07 +0800

    9

--- 9527.py
+++ 9527.py
@@ -1,1 +1,26 @@
-print(1)
+from scapy.all import *
+
+count = 0
+
+
+def traffic_monitor_callbak(pkt):
+    global count
+    if IP in pkt:
+        count += pkt[IP].len
+
+
+def docount():
+    global count
+    sniff(prn=traffic_monitor_callbak, store=0, timeout=1)
+    print(count)
+    count = 0
+
+
+while True:
+    docount()
+
+
+
+
+# sniff(filter='port 1630', prn=lambda x:x.summary(), timeout=10)
+# print(pkt.sprintf("%IP.len%"))
